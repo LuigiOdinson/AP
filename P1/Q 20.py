fe4ac2950e8976490dd35ecd945820eb78105e4f
RominaM <116271810+LuigiOdinson@users.noreply.github.com>
2023-02-17 14:05:17 +0330

    Add files via upload

---
+++ P1/Q 20.py
@@ -0,0 +1,6 @@
+# Write a python program that..
+# Gets the radius of a circle from the user and calculates the area
+r = float(input("Enter the radius: "))
+pi = 3.141592
+area = r**2 * pi
+print("The area of the circle is: ", area)
---
+++ P1/Q 21.py
@@ -0,0 +1,5 @@
+# Write a python program that..
+# Gets the user's first name and last name and prints them
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+print("Hello", f_name, l_name)
